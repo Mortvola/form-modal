99f1bc10a10a24b1c0fc8fbb4d80273cdb1b6dec
Mortvola <44383420+Mortvola@users.noreply.github.com>
2021-11-25 10:14:43 -0700

    Initial commit

---
+++ src/Errors.tsx
@@ -0,0 +1,46 @@
+import React, { CSSProperties, ReactElement } from 'react';
+import PropTypes from 'prop-types';
+
+export interface Error {
+  field: string;
+  message: string;
+  rule: string;
+}
+
+export function setFormErrors(
+  formikSetErrors: (errors: Record<string, string>) => void,
+  errorResponse: Error[],
+): void {
+  const errors: Record<string, string> = {};
+
+  errorResponse.forEach((error) => {
+    errors[error.field] = error.message;
+  });
+
+  formikSetErrors(errors);
+}
+
+let key = 0;
+
+type PropTypes = {
+  errors?: string[],
+  style? : CSSProperties,
+}
+
+const Errors = ({
+  errors,
+  style,
+}: PropTypes): ReactElement => (
+  <div style={style}>
+    {
+      errors
+        ? errors.map((e) => {
+          key += 1;
+          return <div key={key} className="text-danger" style={{ fontSize: 'small' }}>{e}</div>;
+        })
+        : null
+    }
+  </div>
+);
+
+export default Errors;
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = (name, env) => ({
+  name,
+  mode: env.production ? 'production' : 'development',
+  devtool: env.production ? 'source-map' : 'inline-source-map',
+  entry: `./src/index.ts`,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `index.js`,
+    library: {
+      type: 'commonjs2',
+    }
+  },
+  externals: {
+    'react': 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+    'react-bootstrap': 'commonjs react-bootstrap',
+  },
+  module: {
+    rules: [
+      { test: /.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                exportLocalsConvention: 'camelCaseOnly',
+                localIdentName: '[name]-[local]-[hash:base64:5]',
+              },
+              importLoaders: 1,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+        exclude: [/node_modules/, /dist/],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+});
+
+module.exports = [
+  (env) => config('form-modal', env),
+];
