f9b1519a8365a09756ac75b2d24fbf9c70de3d20
Mortvola <mortvola@gmail.com>
2021-11-26 06:32:58 -0700

    Exported more...

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import FormModal from './FormModal';
 import FormField from './FormField';
 import FormError from './FormError';
 import FormTextField from './FormTextField';
+import Errors from './Errors';
 
 export { setFormErrors } from './Errors';
-export { FormModal, FormField, FormError, FormTextField };
+export { FormModal, FormField, FormError, FormTextField, Errors };
