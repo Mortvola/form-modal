c83d494bb0ea52cc6417ce824723cab202edb74b
mortvola <shields12345@msn.com>
2022-06-20 22:01:36 -0700

    Fixed export of SubmitButton

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import FormTextField from './FormTextField';
 import Errors from './Errors';
 import FormRadio from './FormRadio';
 import FormCheckbox from './FormCheckbox';
+import SubmitButton from './SubmitButton';
 
 export { setFormErrors } from './Errors';
-export { FormModal, FormField, FormError, FormTextField, Errors, FormRadio, FormCheckbox };
+export {
+  FormModal, FormField, FormError, FormTextField, Errors, FormRadio, FormCheckbox,
+  SubmitButton,
+};
